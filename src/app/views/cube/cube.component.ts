a9b4702f26aecaec13311776b438435ba2cd477c
Alejandro Sadí <alesf22@gmail.com>
2021-12-01 15:17:49 -0600

    cube component layout created

--- src/app/views/cube/cube.component.ts
+++ src/app/views/cube/cube.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-cube',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CubeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public app: AppService) { }
 
   ngOnInit(): void {
   }
