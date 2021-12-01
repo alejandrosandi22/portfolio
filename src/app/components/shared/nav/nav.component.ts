465a160c353864232321f1c6e92176c754a0389c
Alejandro Sadí <alesf22@gmail.com>
2021-12-01 15:11:50 -0600

    nav component style created

--- src/app/components/shared/nav/nav.component.ts
+++ src/app/components/shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  active: boolean;
+
+  constructor(public app: AppService) {}
 
   ngOnInit(): void {
   }
 
+  toggle(){
+    if(!this.active){
+      this.active = true;
+    }else{
+      this.active = false;
+    }
+  }
+
+  scrolling(){
+    if(this.app.scroll == 0){
+      this.app.scroll = 1;
+    }else if(this.app.scroll == 1){
+      this.app.scroll = 2;
+    }else{
+      this.app.scroll = 0;
+    }
+  }
 }
