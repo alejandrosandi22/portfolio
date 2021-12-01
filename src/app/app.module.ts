c282f3f9eb67f4b535400c17c1230eff9e09ca5a
Alejandro Sadí <alesf22@gmail.com>
2021-12-01 14:58:14 -0600

    components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { CubeComponent } from './views/cube/cube.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    NavComponent,
+    CubeComponent
   ],
   imports: [
     BrowserModule,
