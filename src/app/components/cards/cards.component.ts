0d6321eb0e00ca566ee93d0c8985c28125d2bee9
alesandi22 <alesf22@gmail.com>
2022-04-28 11:33:08 -0600

    fixed performance

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/services/app.service';
-import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-cards',
