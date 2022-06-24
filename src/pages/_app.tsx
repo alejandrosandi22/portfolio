54b7d800d04863ae2d37511c1dd68c8dc3b4fcf5
alejandrosandi22 <alesf22@gmail.com>
2022-06-24 17:59:47 -0600

    google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,32 @@
+/* eslint-disable @next/next/inline-script-id */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import Layout from '../components/layout';
 import { Provider } from 'react-redux';
 import { store } from '../../store';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy='lazyOnload'>
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+    `}
+      </Script>
+
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }
