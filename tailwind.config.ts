19caeaf39f18c095940918febe4d960b2436427f
Alejandro Sandí <alesf22@gmail.com>
2024-12-30 18:35:59 -0600

    style: add project styles and assets

---
+++ tailwind.config.ts
@@ -0,0 +1,93 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        helvetica: ['var(--font-helvetica)', 'Arial', 'sans-serif'],
+        migra: ['var(--font-migra)', 'Times New Roman', 'serif'],
+      },
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        'primary-card': '#0A0A0A',
+        'primary-blue': 'hsl(var(--primary-blue))',
+        'primary-purple': 'hsl(var(--primary-purple))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          hover: '#206BD5',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      height: {
+        main: 'calc(100vh - 80px)',
+      },
+      maxWidth: {
+        default: '1211px',
+      },
+      transitionProperty: {
+        filter: 'filter',
+      },
+      backgroundImage: {
+        'gradient-button':
+          'linear-gradient(45deg, #2327fa, #5727fa, #7628fa, #8e2bfa, #a32ffa)',
+      },
+      backgroundSize: {
+        '300-100': '300% 100%',
+      },
+      keyframes: {
+        glowing: {
+          '0%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+          '100%': { 'background-position': '0% 50%' },
+        },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        glowing: 'glowing infinite 4s alternate',
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
