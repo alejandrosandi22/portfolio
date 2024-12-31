d301f6d59e976fc630af7cce9353c15540def52c
Alejandro Sandí <alesf22@gmail.com>
2024-12-30 18:33:56 -0600

    chore: set up format and configuration files

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
