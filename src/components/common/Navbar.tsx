49beae1f226299335869b04d692e43425aa74367
Alejandro Sandí <alesf22@gmail.com>
2025-01-01 11:02:02 -0600

    fix: mobile navigation

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -11,11 +11,18 @@ import {
 import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
 import { MenuIcon } from 'lucide-react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 function Navbar() {
+  const [isSheetOpen, setSheetOpen] = useState(false);
+
+  const handleCloseSheet = () => {
+    setSheetOpen(false);
+  };
+
   return (
     <header className='absolute top-0 z-50 flex h-20 w-full shrink-0 items-center px-4 md:px-6'>
-      <Sheet>
+      <Sheet open={isSheetOpen} onOpenChange={setSheetOpen}>
         <SheetTrigger asChild>
           <Button variant='outline' size='icon' className='lg:hidden'>
             <MenuIcon className='h-6 w-6' />
@@ -26,7 +33,7 @@ function Navbar() {
           <SheetTitle className='hidden'>
             <VisuallyHidden.Root>x</VisuallyHidden.Root>
           </SheetTitle>
-          <Link href='#' className='mr-6 hidden lg:flex' prefetch={false}>
+          <Link href='/' className='mr-6 hidden lg:flex'>
             <Icon />
             <span className='sr-only'>
               Alejandro Sandí - Front-End & Back-End Developer | Portfolio
@@ -34,36 +41,38 @@ function Navbar() {
           </Link>
           <div className='grid gap-2 py-6'>
             <Link
-              href='#'
+              href='/#home'
               className='flex w-full items-center py-2 text-lg font-semibold'
-              prefetch={false}
+              onClick={handleCloseSheet}
             >
               Home
             </Link>
             <Link
-              href='#'
+              href='/#projects'
               className='flex w-full items-center py-2 text-lg font-semibold'
-              prefetch={false}
+              onClick={handleCloseSheet}
             >
-              About
+              Projects
             </Link>
             <Link
-              href='#'
+              href='/#about-me'
               className='flex w-full items-center py-2 text-lg font-semibold'
-              prefetch={false}
+              onClick={handleCloseSheet}
             >
-              Services
+              About Me
             </Link>
             <div>
-              <Button variant='outline' asChild>
-                <Link href='/#contact'>Let&apos;s talk</Link>
+              <Button variant='outline' className='text-lg' asChild>
+                <Link href='/#contact' onClick={handleCloseSheet}>
+                  Let&apos;s talk
+                </Link>
               </Button>
             </div>
           </div>
         </SheetContent>
       </Sheet>
       <div className='left-0 right-0 m-auto flex w-full max-w-default justify-between'>
-        <Link href='#' className='mr-6 hidden lg:flex' prefetch={false}>
+        <Link href='/#home' className='mr-6 hidden lg:flex'>
           <Icon />
           <span className='sr-only'>
             Alejandro Sandí - Front-End & Back-End Developer | Portfolio
